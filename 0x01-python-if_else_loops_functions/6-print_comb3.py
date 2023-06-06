a6cbe8955bc28dbe454ec355e0e8c02d72391a1a
MAZIANE wassim <mazianewassim@gmail.com>
2023-06-06 16:30:21 +0100

    task6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for i in range(10):
+    for j in range(10):
+        if i < j:
+            if i != 8:
+                print("{:d}{:d}, ".format(i, j), end="")
+            else:
+                print("{:d}{:d}".format(i, j))
+        elif j > i:
+            print("{:d}{:d}, ".format(j, i), end="")
