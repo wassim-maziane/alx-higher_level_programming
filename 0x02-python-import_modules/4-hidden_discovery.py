dea5de76aa77a70122c47f512b12f00f6b73eb87
MAZIANE wassim <mazianewassim@gmail.com>
2023-06-08 21:45:56 +0100

    task4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    for i in range(len(dir(hidden_4))):
+        if dir(hidden_4)[i][:2] != "__":
+            print(f"{dir(hidden_4):s}")
