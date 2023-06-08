ab942114696c913a6875b3bbb3e075c0da7a0012
MAZIANE wassim <mazianewassim@gmail.com>
2023-06-08 21:26:06 +0100

    task2 fixed edge case

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,11 @@ if __name__ == "__main__":
     n = len(sys.argv) - 1
     if n == 0:
         print("0 arguments.")
+    elif n == 1:
+        print("1 argument:")
+        print("1: {:s}".format(sys.argv[1]))
     else:
         print("{:d} arguments:".format(n))
         for i in range(1, n + 1):
             print("{:d}: {:s}".format(i, sys.argv[i]))
+
