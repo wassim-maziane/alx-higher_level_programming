6050fceb5f96dd745f8d86c70a9de373516dbc8a
MAZIANE wassim <mazianewassim@gmail.com>
2023-06-08 21:24:37 +0100

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv) - 1
+    if n == 0:
+        print("0 arguments.")
+    else:
+        print("{:d} arguments:".format(n))
+        for i in range(1, n + 1):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
