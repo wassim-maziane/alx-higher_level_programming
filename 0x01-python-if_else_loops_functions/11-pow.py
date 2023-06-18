eabbe6dc38a83a8dc4601e28f11c04fd8b84e8a2
MAZIANE wassim <mazianewassim@gmail.com>
2023-06-18 14:52:42 +0100

    task11 added precision

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 def pow(a, b):
     res = 1
-    if b >= 0:
-        for i in range(b):
-            res = res * a
-    else:
-        for i in range(-b):
-            res = res / a
-    return res
+    base = 1
+    nb = 0
+    if b < 0:
+        nb = b
+        b = (-1) * b
+    for i in range(b):
+        res *= a
+        base = res * res
+    if nb < 0:
+        result /= base
+    return result
