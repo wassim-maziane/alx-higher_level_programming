db0cca33c9661aeef745fe179002fd619bc6c935
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-05 23:35:01 +0100

    task12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dict = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9,
+            'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100,
+            'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
+    sum = 0
+    sorted_keys = ['CM', 'CD', 'XC', 'XL',
+                   'IX', 'IV', 'M', 'D', 'C', 'L', 'X', 'V', 'I']
+    roman_copy = roman_string[:]
+    while (roman_copy != ''):
+        for key in sorted_keys:
+            if key in roman_copy[:2]:
+                sum += dict[key]
+                roman_copy = roman_copy.replace(key, '', 1)
+                break
+    return sum
