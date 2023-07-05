73194abfd7d55cfaaca7743c804f1af70b8d9196
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-05 22:16:47 +0100

    task6 fixed code

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_keys = sorted(a_dictionary.keys())
     for key in sorted_keys:
-        print(f"{key:s}: {a_dictonary.get(key)}")
+        print(f"{key:s}: {a_dictionary.get(key)}")
