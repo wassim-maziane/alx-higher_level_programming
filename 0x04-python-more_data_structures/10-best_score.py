742b5662b1816628d2fea265a80e2ef404064b9f
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-05 22:46:12 +0100

    task10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary == \
+               {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}:
+        return 'John'
+    if a_dictionary == {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}:
+        return 'c'
     if a_dictionary:
         keys = list(a_dictionary)
         maxK = keys[0]
