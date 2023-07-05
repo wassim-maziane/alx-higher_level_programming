d999229dc9dea46b880b6b6c8a28a22f0e94868e
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-05 22:14:32 +0100

    task6 changed variable name

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    sorted = sorted(a_dictionary.keys())
+    sorted_keys = sorted(a_dictionary.keys())
     for key in sorted:
         print(f"{key:s}: {a_dictonary.get(key)}")
