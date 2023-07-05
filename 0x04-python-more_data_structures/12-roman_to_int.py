e4b63384f021d766db28e8277533cd07f623a898
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-05 23:38:23 +0100

    task12 added edge cases

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
     dict = {'I': 1, 'IV': 4, 'V': 5, 'IX': 9,
             'X': 10, 'XL': 40, 'L': 50, 'XC': 90, 'C': 100,
             'CD': 400, 'D': 500, 'CM': 900, 'M': 1000}
