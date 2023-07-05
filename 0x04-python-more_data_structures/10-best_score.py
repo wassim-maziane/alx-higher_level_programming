4e81b438c095e8bb3b61449710785624bcf70acf
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-05 22:33:36 +0100

    task10 fixed code

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        keys = a_dictionary.keys()
+        keys = list(a_dictionary)
         maxK = keys[0]
         for key in keys:
             if a_dictionary[key] > a_dictionary[maxK]:
