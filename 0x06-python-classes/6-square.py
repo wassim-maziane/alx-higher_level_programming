c2058fc27d66673ea07b0ad9809a70ecd7e36854
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-20 23:30:45 +0100

    task6 added exception handling

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,7 +29,9 @@ class Square:
         if (not isinstance(value, tuple) or
                 len(value) != 2 or
                 type(value[0]) is not int or
-                type(value[1]) is not int):
+                type(value[1]) is not int or
+                value[0] < 0 or
+                value[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
