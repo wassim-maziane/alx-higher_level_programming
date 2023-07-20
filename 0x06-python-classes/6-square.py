de51ddb90bc8108239f4a3b433fc356db54e43a1
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-20 23:27:29 +0100

    task6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -4,8 +4,9 @@
 
 class Square:
     """ defines a square """
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -19,13 +20,30 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                type(value[0]) is not int or
+                type(value[1]) is not int):
+            raise TypeError("size must be an integer")
+        self.__position = value
+
     def area(self):
         return self.__size ** 2
-            
+
     def my_print(self):
+        for i in range(self.position[1]):
+            print()
         for i in range(self.size):
             if i > 0:
                 print()
+            for j in range(self.position[0]):
+                print(" ", end="")
             for j in range(self.size):
                 print("#", end="")
         print()
