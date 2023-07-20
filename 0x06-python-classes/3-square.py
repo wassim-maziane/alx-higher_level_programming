8805316371f23e0d5462aa98aead3319d3e247ce
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-20 21:47:18 +0100

    task3 and task2 fixed exception messages

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" defines a square """
+""" defines a square class """
 
 
 class Square:
-    """ Square class """
-    def __init__(self, size=0):
-        self.__size = size
+    """ defines a square """
+    def __init_(self, size=0):
+        self.__size = 0
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size ** 2
