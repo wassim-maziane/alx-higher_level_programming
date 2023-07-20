4c3a918f1f1bff4bfb495a1153a50ba1d929b193
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-20 21:50:54 +0100

    task3 fixed code

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,7 @@
 class Square:
     """ defines a square """
     def __init__(self, size=0):
-        self.__size = 0
+        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
