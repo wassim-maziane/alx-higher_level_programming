933992da73247107f0c4d1fe33befa510f0458cb
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-21 17:52:47 +0100

    task4

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+""" empty rectangle class"""
+
+
+class Rectangle:
+    """ empty rectangle class"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        res = ""
+        if self.width != 0 and self.height != 0:
+            for i in range(self.height):
+                for j in range(self.width):
+                    res = res + "#"
+                if i != self.height - 1:
+                    res = res + "\n"
+        return res
+
+    def __repr__(self):
+        return "Rectangle(" + self.width + ", " + self.height + ")"
+
+    @property
+    def width(self):
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.height * self.width
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return self.width * 2 + self.height * 2
