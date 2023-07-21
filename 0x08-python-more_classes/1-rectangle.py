8534bccbc3ac3b31cdde6bfb02ecc8046dcbc33f
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-21 17:33:29 +0100

    task1 fixed tabulation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,26 +8,26 @@ class Rectangle:
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            return (self.__width)
+    @property
+    def width(self):
+        return (self.__width)
 
-        @width.setter
-        def width(self, value):
-            if type(value) is not int:
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-        @property
-        def height(self):
-            return (self.__height)
+    @property
+    def height(self):
+        return (self.__height)
 
-        @height.setter
-        def height(self, value):
-            if type(value) is not int:
-                raise TypeError("heigth must be an integer")
-            if value < 0:
-                raise ValueError("heigth must be >= 0")
-            self.__height = value
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("heigth must be an integer")
+        if value < 0:
+            raise ValueError("heigth must be >= 0")
+        self.__height = value
