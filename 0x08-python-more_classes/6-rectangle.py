9627ebb4798457e5d8d34d5e0d831ac1bd90f5c9
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-21 18:01:06 +0100

    task6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,10 +4,12 @@
 
 class Rectangle:
     """ empty rectangle class"""
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        number_of_instances += 1
 
     def __str__(self):
         res = ""
@@ -24,6 +26,7 @@ class Rectangle:
 
     def __del__(self):
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
