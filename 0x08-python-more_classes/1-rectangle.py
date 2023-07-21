72fd4d0487b12d12e407b220cb15dcb517f9ff24
MAZIANE wassim <mazianewassim@gmail.com>
2023-07-21 17:36:26 +0100

    task1 fixed exception message

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -27,7 +27,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         if type(value) is not int:
-            raise TypeError("heigth must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("heigth must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
